7375cf4fb1fd3aee428de6e70c98a36cde991d10
Harita kambhampati <haritastbg@gmail.com>
2019-01-10 14:40:35 -0500

    Update environ_setup.sh

--- ansible_ec2/environ_setup.sh
+++ ansible_ec2/environ_setup.sh
@@ -1,6 +1,6 @@
 ### Installation setup to run ansible and connect to aws
 #!/bin/sh
-sudo apt update
-sudo apt install python-pip -y
+sudo apt-get update
+sudo apt-get install python-pip -y
 pip install boto boto3 
-sudo apt install ansible -y
+sudo apt-get install ansible -y
